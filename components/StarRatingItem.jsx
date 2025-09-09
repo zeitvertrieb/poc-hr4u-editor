5cfa26e7ebb2c7d5f6a6da542248c8d4d9672326
Kaja Kitzmüller <kaja.kitzmueller@ebcont.com>
2025-09-09 13:33:13 +0200

    key fix

--- components/StarRatingItem.jsx
+++ components/StarRatingItem.jsx
@@ -2,7 +2,7 @@ import StarRating from "./StarRating";
 
 export default function StarRatingItem({rating, skills}) {
     return(
-        <div className="flex flex-col gap-1  md:grid md:grid-cols-5">
+        <div key={rating} className="flex flex-col gap-1  md:grid md:grid-cols-5">
             <StarRating rating={rating}/>
             <p className="col-span-4">{skills.join(",")}</p>
         </div>
